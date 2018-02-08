90fbbab7860d0d4c3c54884a2addcecfffbce65c
fifthThirteen <30660189+fifthThirteen@users.noreply.github.com>
2018-02-08 22:48:38 +0800

    1
    1

---
+++ data/index_json.js
@@ -0,0 +1,86 @@
+jsonCallback([
+  {
+    "name": "bootstrap实现响应式",
+    "url":"",
+    "submenu": [
+      {
+        "name": "游览器博物馆",
+        "url":"https://fifththirteen.github.io/browserList-demo/"
+      },
+      {
+        "name": "移动端商城",
+        "url":"",
+        "submenu": [
+          {
+            "name": "主页",
+            "url":"https://fifththirteen.github.io/phonePage/index.html"
+          },
+          {
+            "name": "佣金提现",
+            "url":"https://fifththirteen.github.io/phonePage/financeCenter.html"
+          },
+          {
+            "name": "我的会员",
+            "url":"https://fifththirteen.github.io/phonePage/membersList.html"
+          },
+          {
+            "name": "登录",
+            "url":"https://fifththirteen.github.io/phonePage/login.html"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "css3实现-照片墙",
+    "url":"https://fifththirteen.github.io/css3-pictureShow/"
+  },
+  {
+    "name":"DOM-test-demo",
+    "url":"https://fifththirteen.github.io/domtest/"
+  },
+  {
+    "name":"JS常用效果",
+    "url":"",
+    "submenu": [
+      {
+        "name": "轮播图",
+        "url":"",
+        "submenu": [
+          {
+            "name": "原生JS实现轮播图",
+            "url":"https://fifththirteen.github.io/banner_transition/demo.html"
+          },
+          {
+            "name": "原生JS实现选项卡轮播图",
+            "url":"https://fifththirteen.github.io/banner_transition/demo-change.html"
+          },
+          {
+            "name": "jQuery实现轮播图",
+            "url":"https://fifththirteen.github.io/banner_transition/demo-jq.html"
+          },
+          {
+            "name": "jQuery实现轮播图,css3-transition实现动画",
+            "url":"https://fifththirteen.github.io/banner_transition/demo-transition.html"
+          }
+        ]
+      },
+      {
+        "name": "js选项卡",
+        "url":"https://fifththirteen.github.io/tabSwitch/index.html"
+      },
+      {
+        "name": "js文字向上无缝滚动",
+        "url":"https://fifththirteen.github.io/scrollUp/scroll-up.html"
+      },
+      {
+        "name": "js文字延时向上无缝滚动",
+        "url":"https://fifththirteen.github.io/scrollUp/scroll-up-delay.html"
+      }
+    ]
+  },
+  {
+    "name":"鼎丰居Demo",
+    "url":"https://fifththirteen.github.io/dfj/"
+  }
+]);
